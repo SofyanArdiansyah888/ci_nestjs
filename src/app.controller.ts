a2f181b9636abd84e9be93d1d91adb3d1b925f69
sofyan ardiansyah <sofyanardianysah@gmail.com>
2023-03-21 14:13:39 +0800

    iseng

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/sofyan')
+  hello(): string {
+    return this.appService.getSofyan();
+  }
 }
